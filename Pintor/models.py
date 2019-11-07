7623fd4b45ac15bd86dbcdc7eb5f7e4f8940bbfc
Cesar Mejia <cesarsac@mesoamericana.edu.gt>
2019-11-06 18:22:24 -0600

    se modifico el modelo

--- Pintor/models.py
+++ Pintor/models.py
@@ -2,5 +2,30 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
+
+class Pintor(models.Model):
+
+    nombre  =   models.CharField(max_length=30)
+    Apellido =  models.CharField(max_length=30)
+    fecha_nacimiento = models.DateField()
+    telefono = models.IntegerField()
+
+
+    def __str__(self):
+
+        return self.nombre
+
+class Puntura(models.Model):
+    nombre    = models.CharField(max_length=60)
+    codigo    = models.IntegerField()
+    anio      = models.IntegerField()
+
+
+    actores   = models.ManyToManyField(Actor, through='Actuacion')
+
+    def __str__(self):
+
+        return self.nombre
